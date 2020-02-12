dffebab4759d80a8b08dc1a6e09b62c5e2d621b6
Tom Huang <huangtom@Tom-Huang.local>
2020-02-12 10:03:11 +0800

    Added checkpoints for spider

--- spiders/baiduqianxiSpider.py
+++ spiders/baiduqianxiSpider.py
@@ -4,6 +4,7 @@ import time
 import datetime
 import random
 import json
+from queue import Queue
 
 from scrapy import Request, Selector
 from selenium import webdriver
@@ -15,8 +16,13 @@ SAVE_DIR = os.path.join(PROJECT_ROOT, "rawdata/baiduqianxi/")
 class baiduqianxiSpider():
     def __init__(self):
         self.name = 'baiduqianxi'
-        if not os.path.exists(SAVE_DIR):
-            os.mkdirs(SAVE_DIR)
+        self.crawl_id = datetime.datetime.now().strftime('%S%M%H%d%m%Y')
+        self.SAVE_DIR = os.path.join(SAVE_DIR, self.crawl_id)
+        self.CHECKPOINT_DIR = os.path.join(self.SAVE_DIR, "checkpoint/")
+        if not os.path.exists(self.CHECKPOINT_DIR):
+            os.makedirs(self.CHECKPOINT_DIR)
+
+        self._checkpts = Queue()
         self._browser = webdriver.Chrome(executable_path=WEBDRIVER)
 
     def crawl(self):
@@ -39,6 +45,9 @@ class baiduqianxiSpider():
             '//div[contains(@class,"button_group primary")]/div')
         city_list = self._browser.find_elements_by_xpath(
             '//a[@class="sel_city_name"]')
+        national_element = self._browser.find_element_by_xpath(
+            '//div[@id="hot_city_ids"]/a[@name="全国"]')
+        city_list[0] = national_element
         date_dict = {}
         for idx, date_element in enumerate(date_list):
             assert self._click_element(date_element)
@@ -51,20 +60,21 @@ class baiduqianxiSpider():
                 assert self._click_element(datatype_element)
                 data_type = datatype_element.text
                 print('PARSING: ', data_type)
-                # self._sleep_random(0.05)
+                self._sleep_random(0.05)
                 citydata_dict = {}
                 for city_element in city_list:
                     assert self._click_element(city_element)
                     city = self._browser.find_element_by_id(
                         'cur_city_name').text
                     print('PARSING: ', city)
-                    self._sleep_random(0.05)
+                    self._sleep_random(0.08)
                     table = self._get_table_from_selector(
                         Selector(text=self._browser.page_source))
                     citydata_dict[city] = list(table)
                 datatype_dict[data_type] = citydata_dict
             date_dict[date] = datatype_dict
-        self._dump_json(date_dict)
+            self._checkpoint(date_dict, self.CHECKPOINT_DIR)
+        self._dump_json(date_dict, self.SAVE_DIR)
 
     def _click_element(self, element):
         try:
@@ -94,13 +104,22 @@ class baiduqianxiSpider():
         random.randrange(0, upper_limit*100, 1)/100
         return
 
-    def _dump_json(self, dict):
+    def _checkpoint(self, dict, DIR):
+        checkpt_name = self._dump_json(dict, DIR)
+        self._checkpts.put(checkpt_name)
+        max_checkpt_num = 3
+        if self._checkpts.qsize() >= max_checkpt_num:
+            os.remove(self._checkpts.get())
+        print('CHECKPOINT REACHED: ', checkpt_name)
+
+    def _dump_json(self, dict, DIR):
         file_name = os.path.join(
-            SAVE_DIR,  datetime.datetime.now().strftime("%S%M%H_%d_%m_%Y")+'.json')
+            DIR, datetime.datetime.now().strftime("%S%M%H_%d_%m_%Y")+'.json')
         if os.path.exists(file_name):
             raise ("File already exists")
         with open(file_name, 'w', encoding='utf-8') as f:
             json.dump(dict, f, ensure_ascii=False, indent=4)
+        return file_name
 
 
 if __name__ == "__main__":
